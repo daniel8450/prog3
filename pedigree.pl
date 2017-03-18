af5d7a625e8098715252a5ab73941fce476a1b21
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-17 22:36:48 -0500

    Update pedigree.pl

--- pedigree.pl
+++ pedigree.pl
@@ -2,11 +2,17 @@ male(dan).
 male(jack).
 male(ted).
 male(barney).
+male(alex).
+male(tanner).
+male(nathan).
 
 female(hannah).
 female(robin).
 female(lilly).
 female(mary).
+female(lisa).
+female(sara).
+female(eve).
 
 parent(dan,ted).
 parent(dan, jack).
@@ -14,7 +20,11 @@ parent(hannah, ted).
 parent(hannah, jack).
 parent(barney, mary).
 parent(robin, mary).
+parent(robin, nathan).
+parent(barney, nathan).
 parent(lilly, robin).
+parent(tanner,robin).
+
 
 sibling(A,B):-parent(P,A),parent(P,B),A\=B.
 
@@ -27,13 +37,12 @@ sister(G,S):-female(G),sibling(G,S).
 grandma(M,C):-female(M),parent(M,V),parent(V,C).
 grandpa(P,C):-male(P),parent(P,V),parent(V,C).
 
-aunt(A,C):-female(A),sister(A,P),parent(P,C).
-uncle(U,C):-male(U),brother(U,P),parent(P,C).
+aunt(A,C):-female(A),sibling(A,P),parent(P,C).
+uncle(U,C):-male(U),sibling(U,P),parent(P,C).
 
-neice(N,G):-female(N),aunt(G,N).
-neice(N,G):-female(N),uncle(G,N).
-nephew(N,G):-male(N),aunt(G,N).
-nephew(N,G):-male(N),uncle(G,N).
+neice(N,G):-female(N),sibling(G,P),parent(P,N).
+nephew(N,G):-male(N),sibling(G,P),parent(P,N).
+
 
 
 
