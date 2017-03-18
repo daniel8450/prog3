9443bff547b5beeab291128f9be28ade8d17ee48
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-17 22:27:27 -0500

    Update pedigree.pl

--- pedigree.pl
+++ pedigree.pl
@@ -14,6 +14,7 @@ parent(hannah, ted).
 parent(hannah, jack).
 parent(barney, mary).
 parent(robin, mary).
+parent(lilly, robin).
 
 sibling(A,B):-parent(P,A),parent(P,B),A\=B.
 
@@ -22,6 +23,17 @@ father(F,C):-male(F),parent(F,C).
 
 brother(B,S):-male(B),sibling(B,S).
 sister(G,S):-female(G),sibling(G,S).
+
+grandma(M,C):-female(M),parent(M,V),parent(V,C).
+grandpa(P,C):-male(P),parent(P,V),parent(V,C).
+
+aunt(A,C):-female(A),sister(A,P),parent(P,C).
+uncle(U,C):-male(U),brother(U,P),parent(P,C).
+
+neice(N,G):-female(N),aunt(G,N).
+neice(N,G):-female(N),uncle(G,N).
+nephew(N,G):-male(N),aunt(G,N).
+nephew(N,G):-male(N),uncle(G,N).
 
 
 
