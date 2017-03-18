1de3ff22159f46dec8676f9ecde39f7b6c71202a
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-18 09:35:59 -0500

    Update multimap.hs

--- multimap.hs
+++ multimap.hs
@@ -18,7 +18,7 @@ let x = [1,2,3]
 let y = [6,7,3]
 let z = filterr (>1) x
 let w = mapp (+3) y
-let m = (multimap filterr (>1) [5,6,7])
+let m = (multimap (filterr (>1) [5,6,7]) [1,2,3])
 print z
 print w
 print m
