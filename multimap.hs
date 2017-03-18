6126ae6f5a02fbdbb93776c2e6e62009aacf4893
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-18 08:38:08 -0500

    Update multimap.hs

--- multimap.hs
+++ multimap.hs
@@ -8,17 +8,17 @@ filterr f (x:xs)
   | f x      = x : (filterr f xs)
   | otherwise = filterr f xs
   
---multimap :: (a -> [a]) -> [a] -> [[a]]
---multimap _ [] = []
---multimap n (x:xs) (y:ys) =  n x y : multimap mapp n xs ys
+multimap :: (a -> [a]) -> [a] -> [[a]]
+multimap _ [] = []
+multimap n (x:xs) (y:ys) =  n x y : multimap mapp n xs ys
 
 main = do
 let x = [1,2,3]
 let y = [6,7]
 let z = filterr (==2) x
 let b = mapp (+3) y
---let m = multimap((map (+3)) x y)
+let m = multimap((map (+3)) x y)
 print z
 print b
---print m
+print m
 
