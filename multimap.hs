5c57d38881691cd80f16655eef35ffa13e439f5e
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-18 10:31:03 -0500

    Update multimap.hs

--- multimap.hs
+++ multimap.hs
@@ -10,7 +10,7 @@ filterr f (x:xs)
   
 multimap :: (a -> [a]) -> [a] -> [[a]]
 multimap _ [] = []
-multimap f (x:xs) =  mapp f x : multimap (mapp f) xs
+multimap f (x:xs) =  f x : multimap f xs
 
           
 
