58a7a1e37971c03e6fdb1122ad64d55ace099b28
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 10:25:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 
 echo
 echo "///pedigree///"
-echo $prolog
-echo $consult('pedigree.pl').
-echo $father(X,mary).
-echo $;
-echo $grandma(X,mary).
-echo $;
-echo $aunt(X,mary).
-echo $;
-echo $neice(X,dan).
-echo $;
+prolog
+consult('pedigree.pl').
+father(X,mary).
+;
+grandma(X,mary).
+;
+aunt(X,mary).
+;
+neice(X,dan).
+;
 
 echo
 echo "///multimap///"
