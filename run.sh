da5f6d1d3aaa3ec31fdd6cea994fdaf31e650ca3
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 10:40:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
+echo
+echo "///multimap///"
+./multimap
+
 echo  
 echo "///pedigree///"
-echo ///to test use:///
+echo "///to test use:///"
 echo "//father(X,mary).///"
 echo "///grandma(X,mary).///" 
 echo "///aunt(X,mary).///"
 echo "///neice(X,dan).///"
 prolog
-
-
-echo
-echo "///multimap///"
-./multimap
-
-
