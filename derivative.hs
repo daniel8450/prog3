d78e41776bf594098ed5c7cfd37ba4d9cdfaa5f0
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 16:21:54 -0500

    Update derivative.hs

--- derivative.hs
+++ derivative.hs
@@ -2,7 +2,7 @@ dd :: (Float -> Float) -> Float
 dd fx = ((f(x+0.1))-(f(x-0.1)))/(.02)
 
 
-dx :: (Float - > Float) -> (Float -> Float)
+dx :: (Float -> Float) -> (Float -> Float)
 dx n = if (n%1==0) then = dx 0
        else = dd n
        
