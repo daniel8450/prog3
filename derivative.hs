6db1cb3347c22b83131687b2368578de6b18050c
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 16:23:04 -0500

    Update derivative.hs

--- derivative.hs
+++ derivative.hs
@@ -3,8 +3,8 @@ dd fx = ((f(x+0.1))-(f(x-0.1)))/(.02)
 
 
 dx :: (Float -> Float) -> (Float -> Float)
-dx n = if (n%1==0) then = dx 0
-       else = dd n
+dx n = if (n%1==0) then dx 0
+       else dd n
        
 main = do
 let m = dx 3
