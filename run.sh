706688849b8b32c4697683bba6185a72ff6afd3e
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 10:37:08 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 echo  
 echo "///pedigree///"
-echo "//to test use://
-echo "//father(X,mary).//"
-echo "//grandma(X,mary).//" 
-echo "//aunt(X,mary).//"
-echo "//neice(X,dan).//"
+echo "///to test use:///
+echo "///father(X,mary).///"
+echo "///grandma(X,mary).///" 
+echo "///aunt(X,mary).///"
+echo "///neice(X,dan).///"
 prolog
 
 
