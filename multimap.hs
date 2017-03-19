5030634a26092fff89c4cc6b4b0af52f50013a7a
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 04:43:10 -0500

    Update multimap.hs

--- multimap.hs
+++ multimap.hs
@@ -18,7 +18,7 @@ let x = [1,2,3]
 let y = [6,7,3]
 let z = filterr (>1) x
 let w = mapp (+3) y
-let m = multimap (map ((filterr odd) y))
+let m = multimap (map (filterr odd y) x) z
 print z
 print w
 print m
