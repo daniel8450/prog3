30b156e38ec0dc145d409424a2784d3584162b9b
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 04:32:32 -0500

    Update multimap.hs

--- multimap.hs
+++ multimap.hs
@@ -13,7 +13,7 @@ multimap _ [] = []
 multimap f (x:xs) = f x : multimap f xs ++ [xs]
 
 mM :: [a] -> [[a]]
-mM y = multimap (filterr (>2) x) y
+mM y = multimap (filterr (>2)) y
     where x = [1,2,3,4,5]
 
           
