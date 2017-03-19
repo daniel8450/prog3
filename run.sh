48788f22d25d42fc7bde317e0d3ebd771fd66856
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 10:36:10 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 echo  
 echo "///pedigree///"
 echo "//to test use://
-echo "father(X,mary)."
-echo "grandma(X,mary)." 
-echo "aunt(X,mary)."
-echo "neice(X,dan)."
+echo "//father(X,mary).//"
+echo "//grandma(X,mary).//" 
+echo "//aunt(X,mary).//"
+echo "//neice(X,dan).//"
 prolog
-echo ouch
+
 
 echo
 echo "///multimap///"
