07d13e52869e8e031e578eb73839c5eb5707c7ef
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 16:19:13 -0500

    Create derivative.hs

---
+++ derivative.hs
@@ -0,0 +1,15 @@
+dd :: (Float -> Float) -> Float
+dd fx = ((f(x+0.1))-(f(x-0.1)))/(.02)
+
+
+dx :: (Float - > Float) -> (Float -> Float)
+dx n = if (n%1==0) then = dx 0
+       else = dd n
+       
+main = do
+let m = dx 3
+print m
+let w = dx 3x
+print w
+
+       
