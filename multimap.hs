5b0011f4026a348783f9ce45102d2d935efa387d
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 09:50:35 -0500

    Update multimap.hs

--- multimap.hs
+++ multimap.hs
@@ -20,9 +20,9 @@ multimapp n x = map (map (n)) x
 main = do
 let x = [1,2,3]
 let y = [6,7,3]
---let z = filterr (>1) x
+let z = filterr (>1) x
 let w = mapp (+3) y
-let m = multimapp (+3) x
+let m = multimapp mM x
 --print z
 --print w
 print m
