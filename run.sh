c5fe6d81b1d61058ecf583e3f6fa650a0ebc51e5
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 10:35:21 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo  
-echo "///                                 pedigree                                            ///"
-echo "///to test use ->    father(X,mary).   grandma(X,mary).   aunt(X,mary).   neice(X,dan).///"
-echo
+echo "///pedigree///"
+echo "//to test use://
+echo "father(X,mary)."
+echo "grandma(X,mary)." 
+echo "aunt(X,mary)."
+echo "neice(X,dan)."
 prolog
 echo ouch
 
