7a0fb83915611942ea207a674f2b90b4f959398a
Daniel LaBorde <dlabo14@lsu.edu>
2017-03-19 10:38:53 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 echo  
 echo "///pedigree///"
 echo "///to test use:///"
-echo "//father(X,mary)///
+echo "//father(X,mary)///"
 echo "///grandma(X,mary)///" 
 echo "///aunt(X,mary)///"
 echo "///neice(X,dan)///"
